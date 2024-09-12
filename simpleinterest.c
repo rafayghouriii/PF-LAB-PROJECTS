837f09ff1f40d5ecab878d855e1bc6cf37f7f633
rafayghouriii <ghourirafay16@gmail.com>
2024-09-12 20:37:50 +0500

    Add files via upload

---
+++ simpleinterest.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    float simpleinterest,principle,rate,period;
+
+    printf("Enter Principle between 100 to 1000000\n");
+
+    scanf("%f",&principle);
+
+    printf("Enter Rate between 5% to 10%\n");
+
+    scanf("%f",&rate);
+
+    printf("Enter Period between 1 year to 10 years\n");
+
+    scanf("%f",&period);
+
+    simpleinterest=principle*rate/100 *period;
+
+    printf("Simple Interest is %f",simpleinterest);
+
+    
+
+
+    return 0;
+
+}
---
+++ taxratesalary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(){
+
+  float taxrate=0,salary=0,tax=0,newsalary=0;
+
+  printf("Enter tax rate");
+
+  scanf("%f",&taxrate);
+
+  printf("Enter salary");
+
+  scanf("%f",&salary);
+
+  tax=taxrate*salary;
+
+  printf("Tax to be paid is %f",tax);
+
+  newsalary=salary-tax;
+
+  printf("New salary is %f",newsalary);
+
+ 
+
+    return 0;
+
+}
